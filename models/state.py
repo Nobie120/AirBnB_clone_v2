10a3829f5c89105a450e03484798ba7678fa97f7
Nobie120 <n12507400@gmail.com>
2024-03-20 07:58:56 -0700

    database

--- models/state.py
+++ models/state.py
@@ -15,6 +15,7 @@ class State(BaseModel, Base):
         name: input name
     """
     __tablename__ = "states"
+
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade='all, delete, delete-orphan',
                           backref="state")
